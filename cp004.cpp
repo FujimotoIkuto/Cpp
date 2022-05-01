58b4b50ff8e582584c98232cc8d89ddff844ffdc
Fujimoto Ikuto <r118157@g.kobe-kosen.ac.jp>
2022-05-01 13:28:15 +0900

    [1-4]数字と文字列を入力するプログラム

---
+++ cp004.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int dt;
+    string ss;
+
+    cout << "数値の入力:";
+    cin >> dt;
+    cout << "文字列の入力:";
+    cin >> ss;
+
+    cout << "数値は" << dt << "です\n";
+    cout << "文字列は" << ss << "です\n" ;   
+}
